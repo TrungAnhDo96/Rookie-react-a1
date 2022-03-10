8a46ece8233d412154830c356adbac95beb817ce
TrungAnhDo96 <trunganhdo96@gmail.com>
2022-03-10 08:37:53 +0700

    Implement login with remember me, login validation WIP

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Form, Button, Spinner } from "react-bootstrap";
 import { AuthContext } from "../../App";
 import "./index.css";
 
 function LoginForm() {
-  const initialState = {
+  const initialLoginDetails = {
     email: "",
     password: "",
+  };
+  const initialState = {
+    isLoaded: false,
     isValidated: false,
     isSubmitting: false,
     errorMessage: null,
   };
   const { state, dispatch } = React.useContext(AuthContext);
+  const [detail, setDetails] = useState(initialLoginDetails);
   const [data, setData] = useState(initialState);
   const [isToBeRemembered, setIsToBeRemembered] = useState(false);
 
@@ -27,7 +31,7 @@ function LoginForm() {
   }
 
   function handleInputChange(event) {
-    setData({ ...data, [event.target.name]: event.target.value });
+    setDetails({ ...detail, [event.target.name]: event.target.value });
   }
 
   function handleLogout() {
@@ -35,14 +39,12 @@ function LoginForm() {
       type: "LOGOUT",
       payload: null,
     });
-    setData(...initialState);
+    setData({ ...initialState });
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  function handleLogin() {
     setData({
       ...data,
-      isValidated: checkValidity(event.currentTarget),
       isSubmitting: true,
     });
     fetch("https://60dff0ba6b689e001788c858.mockapi.io/tokens")
@@ -57,12 +59,40 @@ function LoginForm() {
           type: "LOGIN_SESSION",
           payload: result,
         });
+        if (isToBeRemembered === true) {
+          dispatch({
+            type: "LOGIN_LOCAL",
+            payload: result,
+          });
+        }
         setData({ ...data, isSubmitting: false });
       })
       .catch((e) => {
         setData({ ...data, isSubmitting: false, errorMessage: e });
       });
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setData({
+      ...data,
+      isValidated: checkValidity(event.currentTarget),
+    });
+    handleLogin();
+  }
+
+  useEffect(() => {
+    const localUserId = localStorage.getItem("userId");
+    const localToken = localStorage.getItem("token");
+    if (localToken !== null && data.isLoaded === false) {
+      setData({ ...data, isValidated: true });
+      dispatch({
+        type: "LOGIN_SESSION",
+        payload: { userId: localUserId, token: localToken },
+      });
+    }
+    setData({ ...data, isLoaded: true });
+  }, []);
 
   return (
     <div className="loginForm">
